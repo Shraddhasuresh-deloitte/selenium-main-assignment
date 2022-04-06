491fd480a1f6b632ccc6dfaf1957d28240cd0828
Suresh <shrasuresh@deloitte.com>
2022-04-06 18:02:41 +0530

    test2 uodated

---
+++ alert1.py
@@ -0,0 +1,35 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+driver = webdriver.Chrome("C:\\Users\\shrasuresh\\Downloads\\chromedriver\\chromedriver.exe")
+driver.get("https://shraddhas-trials7401.orangehrmlive.com/client/#/dashboard")
+driver.find_element(By.ID, "txtUsername").send_keys("Admin")
+driver.find_element(By.ID, "txtPassword").send_keys("ry@bL4S3KN")
+button = driver.find_element(By.CLASS_NAME, "button-holder")
+button.click()
+
+
+# driver = webdriver.Chrome("C:\\Users\\shrasuresh\\Downloads\\chromedriver\\chromedriver.exe")
+def alert_pop():
+    driver.get("https://shraddhas-trials7401.orangehrmlive.com/client/#/dashboard")
+
+
+# sel = Select(driver.find_element(By.CLASS_NAME, "dropdown-button waves-effect active"))
+# sel.select_by_visible_text('About')
+button = driver.find_element(By.ID, "account-name")
+button.click()
+button = driver.find_element(By.ID, "aboutDisplayLink")
+button.click()
+driver.implicitly_wait(5)
+button = driver.find_element(By.ID, "heartbeatSubmitBtn")
+button.click()
+driver.implicitly_wait(5)
+
+
+
+
+
